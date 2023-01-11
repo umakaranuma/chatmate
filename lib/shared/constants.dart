a2c11fb8a8dd78b95bdb0d953cebd2cea7379563
umakaranuma <a.umakaran1126@gmail.com>
2023-01-11 23:01:42 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:926250745350:web:46c4d965022e7af71d4d87";
+  static String apiKey = "AIzaSyB4gZ3iDAjn0WQfC75mHwfcOuJvSCPqb1U";
+  static String messagingSenderId = "926250745350";
+  static String projectId = "chatapp-flutter-d32a7";
+  final primaryColor = Color.fromARGB(255, 69, 2, 255);
+}
